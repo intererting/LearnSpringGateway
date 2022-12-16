4cf58316c2a0de1a44680731e5dc7841ad21bdfa
yuliyang <645644717@qq.com>
2022-12-16 17:39:10 +0800

    Initial commit

---
+++ Server/src/main/java/com/example/server/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(@RequestParam("name") String name) {
+        return "test from server " + name;
+    }
+
+}
